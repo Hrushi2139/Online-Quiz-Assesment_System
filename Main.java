fa18b5174b616ea3f16cc37005868895244af32d
Hrushikesh <sannidhanamhrushikesh@gmail.com>
2026-04-20 16:30:37 +0530

    Main file Updated

--- Main.java
+++ Main.java
@@ -2,24 +2,8 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-
-        ArrayList<Question> questions = new ArrayList<>();
-        questions.add(new Question(
-            "What is the size of int in Java?",
-            new String[]{"2 bytes","4 bytes","8 bytes","Depends on system"},
-            2
-        ));
-        questions.add(new Question(
-            "What Keyword is used to inherit a class in Java?",
-            new String[]{"this","super","extends","implements"},
-            3
-        ));
-        questions.add( new Question(
-            "What method is entry point of Java Program?",
-            new String[]{"start()","run()","main()","init()"},
-            3
-        ));
-        Collections.shuffle(questions); 
+        FileHandler fr=new FileHandler();
+        ArrayList<Question> questions = fr.loadQuestions("questions.txt");
         QuizManager quizManager = new QuizManager();
         quizManager.startQuiz(questions);
     }
