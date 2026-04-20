d2f9009286427ae74332148338a3d1c4f2e273bf
Hrushikesh <sannidhanamhrushikesh@gmail.com>
2026-04-20 17:49:25 +0530

    Admin can add questions

--- FileHandler.java
+++ FileHandler.java
@@ -1,6 +1,8 @@
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.util.ArrayList;
 
 public class FileHandler {
@@ -21,5 +23,25 @@ public class FileHandler {
             System.out.println("Error in File");
         }
         return questions;
+    }
+    public void appendQuestion(String file,Question que){
+       try {
+           BufferedWriter bw = new BufferedWriter(new FileWriter(file,true));
+           StringBuilder Stringline=new StringBuilder();
+           Stringline.append(que.getQuestion()).append("|");
+           String [] options = que.getOptions();
+           for(int i=0;i<options.length;i++){
+                Stringline.append(options[i]);
+                if(i!=options.length-1) Stringline.append(",");
+           }
+           Stringline.append("|");
+           Stringline.append(que.getCorrectOption());
+           bw.write(Stringline.toString());
+           bw.newLine();
+           bw.close();
+           System.out.println("Question Added Successfully");
+       } catch (Exception e) {
+            System.out.println("Error in adding the Question");
+       }
     }
 }
--- Main.java
+++ Main.java
@@ -2,9 +2,34 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner  in = new Scanner(System.in);
+        System.out.println("1. Quiz");
+        System.out.println("2. Add Question - Admin");
+        System.out.println("Enter the Choice :");
+        int ch = in.nextInt();
+        in.nextLine();
         FileHandler fr=new FileHandler();
-        ArrayList<Question> questions = fr.loadQuestions("questions.txt");
-        QuizManager quizManager = new QuizManager();
-        quizManager.startQuiz(questions);
+        if(ch==1){
+            ArrayList<Question> questions = fr.loadQuestions("questions.txt");
+            QuizManager quizManager = new QuizManager();
+            quizManager.startQuiz(questions);
+
+        }else if(ch==2){
+            System.out.println("Enter question: ");
+            String text=in.nextLine();
+            String [] opts=new String[4];
+            for(int i=0;i<4;i++){
+                System.out.print("Enter option "+(i+1) + ": ");
+                opts[i]=in.nextLine();
+            }
+            System.out.println("Enter correct option : ");
+            int crtOpt=in.nextInt();
+            if(crtOpt<1 || crtOpt>4){
+                System.out.println("Invalid Option");
+                return;
+            }
+            Question que= new Question(text,opts,crtOpt);
+            fr.appendQuestion("questions.txt",que);
+        }
     }
 }
